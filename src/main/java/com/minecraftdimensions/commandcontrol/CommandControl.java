16f2192b52b5ae587685cb269ae8a4e1c18faadf
bloodsplat <pure__instinct@hotmail.com>
2013-11-12 22:59:57 +1100

    No longer registers commands

--- src/main/java/com/minecraftdimensions/commandcontrol/CommandControl.java
+++ src/main/java/com/minecraftdimensions/commandcontrol/CommandControl.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import com.minecraftdimensions.command.config.Config;
 
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class CommandControl extends Plugin {
@@ -12,36 +13,37 @@ public class CommandControl extends Plugin {
 
 	private Config config;
 
-	public HashMap<String, String> serverCommands = new HashMap<String, String>();
-	public HashMap<String, String> proxyCommands = new HashMap<String, String>();
+	public HashMap<String, String> serverCommands = new HashMap<>();
+	public HashMap<String, String> proxyCommands = new HashMap<>();
 
 	public void onEnable() {
 		proxy = ProxyServer.getInstance();
 		getConfig();
-		registerCommands();
+		registerListener();
 	}
 
-	private void getConfig() {
+    private void registerListener() {
+
+        proxy.getPluginManager().registerListener( this, new CommandListener() );
+
+    }
+
+    private void getConfig() {
 		String configpath = "/plugins/CommandControl/commands.yml";
 		config = new Config(configpath);
 		for (String data : config.getSubNodes("ServerCommands")) {
+            if(!data.startsWith( "/" )){
+                data="/"+data;
+            }
 			serverCommands.put(data,
 					config.getString("ServerCommands." + data, null));
 		}
 		for (String data : config.getSubNodes("BungeeCommands")) {
+            if(!data.startsWith( "/" )){
+                data="/"+data;
+            }
 			proxyCommands.put(data,
 					config.getString("BungeeCommands." + data, null));
-		}
-	}
-
-	private void registerCommands() {
-		for (String data : serverCommands.keySet()) {
-			ProxyServer.getInstance().getPluginManager()
-					.registerCommand(this, new AliasCommand(this, data));
-		}
-		for (String data : proxyCommands.keySet()) {
-			ProxyServer.getInstance().getPluginManager()
-					.registerCommand(this, new PAliasCommand(this, data));
 		}
 	}
 
